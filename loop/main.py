df6a85dc80d94af05c151a645a868d0219680e41
Mateus Vinicios Guimarães Rios de Assis <mateusvinicios214@gmail.com>
2026-03-19 17:29:29 -0300

    loop

---
+++ loop/main.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a # between 1 - 10: "))
+
+while number < 1 or number > 10:
+    print(f"{number} is not valid")
+    number = int(input("Enter a # between 1 - 10: "))
+
+print(f"Your number is {number}")
